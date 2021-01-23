b7b3765646c165f3c9ed247e14a41c6be4fe6875
Alan Mir <alanpaulphotos@gmail.com>
2021-01-23 15:25:08 -0500

    Added state and handleChanges function to the useForm hook

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,16 @@
 // write your custom hook here to control your checkout form
+import e from "cors";
+import { useState } from "react";
+
+const useForm = (initialValue) => {
+
+    const [values, setValues] = useState(initialValue);
+    const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+
+    const handleChanges = () => {
+        setValues({
+            ...values,
+            [e.target.name] : e.target.value
+        });
+    };
+};
