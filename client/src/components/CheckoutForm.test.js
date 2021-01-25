301dd54189293e558521d41edca99ab37455220d
Alan Mir <alanpaulphotos@gmail.com>
2021-01-24 23:12:26 -0500

    Finished the second part of testing

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,7 +1,7 @@
 // Write up the two tests here and make sure they are testing what the title shows
 
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent, expect } from '@testing-library/react';
 import CheckoutForm from './CheckoutForm';
 
 test('form header renders', () => {
@@ -14,4 +14,36 @@ test('form header renders', () => {
 
 test('form shows success message on submit with form details', () => {
     render(<CheckoutForm />);
+
+    const firstNameInput = screen.getByLabelText(/first name/i);
+    const lastNameInput = screen.getByLabelText(/last name/i);
+    const addressInput = screen.getByLabelText(/address/i);
+    const cityInput = screen.getByLabelText(/city/i);
+    const stateInput = screen.getByLabelText(/state/i);
+    const zipInput = screen.getByLabelText(/zip/i);
+
+    fireEvent.change(firstNameInput, {target: {value: "Alan"}});
+    fireEvent.change(lastNameInput, {target: {value: "Mir"}});
+    fireEvent.change(addressInput, {target: {value: "3002 Atlantis Lane"}});
+    fireEvent.change(cityInput, {target: {value: "Olypmus"}});
+    fireEvent.change(stateInput, {target: {value: "Narnia"}});
+    fireEvent.change(zipInput, {target: {value: 30042}});
+
+    expect(firstNameInput).toHaveValue("Alan");
+    expect(lastNameInput).toHaveValue("Mir");
+    expect(addressInput).toHaveValue("3002 Atlantis Lane");
+    expect(cityInput).toHaveValue("Olypmus");
+    expect(stateInput).toHaveValue("Narnia");
+    expect(zipInput).toHaveValue("30042");
+
+    expect(lastNameInput).not.toHaveValue("Alex");
+    expect(cityInput).not.toHaveValue("Asgard");
+    expect(zipInput).not.toHaveValue("333333");
+
+    const checkoutButton = screen.getByRole("button", /checkout/i);
+    fireEvent.click(checkoutButton);
+
+    const order = await screen.findByTestId("successMessage");
+    expect(order).toBeTruthy();
+
 });
